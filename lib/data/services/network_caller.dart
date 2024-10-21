a308b3f7daa3097a91503c770e965c22d70a949a
Rizwan Ansari <rizwangazi7466@gmail.com>
2024-10-21 08:19:43 +0530

    v 1.0.0

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+import 'package:task_manager/data/models/network_response.dart';
+
+class NetworkCaller {
+  static Future<NetworkResponse> getRequest(String url) async {
+    try {
+      Uri uri = Uri.parse(url);
+      debugPrint('Calling URL: $uri');
+      final Response response = await get(uri);
+
+      printResponse(uri.toString(), response);
+
+      if (response.statusCode == 200) {
+        final decodedData = jsonDecode(response.body);
+
+        return NetworkResponse(
+          isSuccess: true,
+          statusCode: response.statusCode,
+          responseData: decodedData,
+        );
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      return NetworkResponse(
+        isSuccess: false,
+        statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  static Future<NetworkResponse> postRequest(
+      String url, Map<String, dynamic>? body) async {
+    try {
+      Uri uri = Uri.parse(url);
+      debugPrint('Calling URL: $uri');
+      final Response response = await post(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+
+      printResponse(uri.toString(), response);
+
+      if (response.statusCode == 200) {
+        final decodedData = jsonDecode(response.body);
+
+        return NetworkResponse(
+          isSuccess: true,
+          statusCode: response.statusCode,
+          responseData: decodedData,
+        );
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      return NetworkResponse(
+        isSuccess: false,
+        statusCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  static void printResponse(String uri, Response response) {
+    debugPrint(
+        'Called URL: $uri\nResponse Code: ${response.statusCode}\nResponse Body: ${response.body}');
+  }
+}
